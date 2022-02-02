5b36aeee56fba1c2530b40bbd9b7d57a3cff456a
Mehdi Mohammadpour <mm.mohammadpour@gmail.com>
2022-02-03 00:17:27 +0100

    allowed hosts added

--- inventoryManagerWS/settings.py
+++ inventoryManagerWS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-k2*_*o@!b25ecwf3^ewbrth*zze74^xuintedledy!9+d^$b0%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ims-ws.herokuapp.com']
 
 
 # Application definition
